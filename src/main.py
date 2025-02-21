fd180ebcf6eac9350dc2f8007e2b97af4a3a3741
nibilis <nhenriquesdealmeida@gmail.com>
2025-02-21 16:46:52 -0300

    Added cron job to main.py

--- src/main.py
+++ src/main.py
@@ -16,23 +16,39 @@ starkbank.language = "en-US"
 
 # Webhook route
 @app.route("/webhook", methods=["POST"])
+# Function that receives a webhook for an invoice data. If the event type
+# is 'credited', it transfers the invoice value to the Stark Bank account
+# and returns http code 200. If the event type is not 'credited', the function
+# just returns 200
 def webhook():
-    # Checking if the request is by POST method
-    if request.method == "POST":
-        # Checking if the request is sent by Stark Bank
-        event = starkbank.event.parse(
-            content=request.data.decode("utf-8"),
-            signature=request.headers["Digital-Signature"],
-        )
-        # Checking the invoice status
-        if event.log.invoice.status == "credited":
-            transfer_to_starkbank(event.log.invoice.amount)
-            print("Transferencia realizada!")
-            return "success", 200
+    # Checking if the request is sent by Stark Bank
+    signature = request.headers.get("Digital-Signature")
+    if not signature:
+        abort(400, "Missing Digital-Signature")
+
+    event = starkbank.event.parse(
+        content=request.data.decode("utf-8"),
+        signature=signature,
+    )
+    # Checking the invoice status
+    if event.log.invoice.status == "credited":
+        transfer_to_starkbank(event.log.invoice.amount)
+        print("Money was transferred to Stark Bank!")
+        return "Success", 200
     else:
-        abort(400)
+        return "", 200
+
+# Cron job route
+@app.route("/cron", methods=["GET"])
+# Function that receives a request and calls the function to create
+# invoices (8-12 by default). This function is scheduled to happen every
+# 3 hours by a cron job
+def cron_send_invoices():
+    action = request.args.get("action")
+    if action == "send_invoices":
+        create_invoices()
+        return "Invoices sent", 200
+    return "Invalid action", 400
 
 if __name__ == "__main__":
-    app.run()
-
-    
+    app.run(debug=True)
--- tests/test_webhook.py
+++ tests/test_webhook.py
@@ -6,3 +6,5 @@ webhook_url = "http://127.0.0.1:5000/webhook"
 data = { "status": "credited" }
 
 r = requests.post(webhook_url, data=json.dumps(data), headers={"Content-Type": "application/json"})
+
+print(r.content)
